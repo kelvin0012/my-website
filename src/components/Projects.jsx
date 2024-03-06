1f68458bcf2d4b6332dcbf972c7d6422453f1939
kelvin0012 <kelvin.at.thesky@gmail.com>
2024-03-06 12:40:16 +1100

    update projects timeline

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,6 +8,7 @@ import LanguageIcon from '@mui/icons-material/Language';
 import accentureLogo from "../assets/accentureLogo.jpg";
 import wsuLogo from "../assets/wsuLogo.jpg"
 import websiteIcon from "../assets/websiteIcon.png"
+import unihack2024Icon from "../assets/unihack2024Icon.png"
 
 
 
@@ -64,6 +65,34 @@ export const Projects = () => {
                         </h3>
                         <p> This very site you are using right now!!</p>
                         <p className="Tech">Tech Used: React</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement 
+                    className="vertical-timeline-element--education" 
+                    date={<span style={{ color: "white" }}>March 2024</span>} 
+                    iconStyle={{background: "white"}} 
+                    icon={<img className="websiteIcon"
+                    src={unihack2024Icon}
+                    alt="unihack2024 Icon Image"
+                    />}>
+                        <h3 className="vertica-timeline-element-title">
+                            Atlassian x UNIHACK
+                        </h3>
+                        <p> Developed a slackbot that uses AI to summarise conversations from the past number of days, hours, or messages and displays the summary on a website</p>
+                        <p className="Tech">Tech Used: React, Python</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement 
+                    className="vertical-timeline-element--education" 
+                    date={<span style={{ color: "white" }}>March 2024 - </span>} 
+                    iconStyle={{background: "white"}} 
+                    icon={<img className="websiteIcon"
+                    src={wsuLogo}
+                    alt="WSU Icon Image"
+                    />}>
+                        <h3 className="vertica-timeline-element-title">
+                            Web Developer Intern
+                        </h3>
+                        <p> Currently a part of the Developer team for the Work Place Management Information System Project</p>
+                        <p className="Tech">Tech Used: React, Python</p>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
             </div>
