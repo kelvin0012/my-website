0ad5b8361617c0135afa618cad6843baf3f9107a
kelvingian <kelvin.gian@optus.com.au>
2025-04-11 10:18:19 +1000

    Fix squished skills icons in mobile view

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
-import { Container } from "@mui/material"
-import { Box, Grid } from "@mui/material"
+import { Container } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import cppIcon from "../assets/cppIcon.png";
@@ -15,12 +15,12 @@ import Tooltip from '@mui/material/Tooltip';
 export const Skills = () => {
     return (
         <div className="skills">
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2}>
                 <Grid item xs={12}>
                     <h2 className="skillsTitle">SKILLS</h2>
                     <p className="hoverText">Hover over icons for more details</p>
                 </Grid>
-                <Grid item xs={1}>
+                <Grid item xs={6} sm={3} md={1}>
                     <Tooltip title="Strong foundation" placement="top" PopperProps={{
                         popperOptions: {
                         modifiers: [
@@ -37,7 +37,7 @@ export const Skills = () => {
                         <p>C++</p>
                     </Tooltip>
                 </Grid>
-                <Grid item xs={1}>
+                <Grid item xs={6} sm={3} md={1}>
                     <Tooltip title="Extensive experience" placement="top" PopperProps={{
                         popperOptions: {
                         modifiers: [
@@ -54,34 +54,30 @@ export const Skills = () => {
                     <p>Python</p>
                     </Tooltip>
                 </Grid>
-                    <Grid item xs={1}>
-                        <img className="skillsIcon" src={reactIcon} alt="React Icon Image"/>
-                        <p>React</p>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <img className="skillsIcon" src={jsIcon} alt="JavaScript Icon Image"/>
-                        <p>JavaScript</p>
-                    </Grid>
-        
-                    <Grid item xs={1}>
-                        <img className="skillsIcon" src={htmlIcon} alt="HTML Icon Image"/>
-                        <p>HTML</p>
-                    </Grid>
-                
-                    <Grid item xs={1}>
-                        <img className="skillsIcon" src={cssIcon} alt="CSS Icon Image"/>
-                        <p>CSS</p>
-                    </Grid>
-                
-                    <Grid item xs={1}>
-                        <img className="skillsIcon" src={sqlIcon} alt="SQL Icon Image"/>
-                        <p>SQL</p>
-                    </Grid>
-                
-                    <Grid item xs={1}>
-                        <img className="skillsIcon" src={dynamodbIcon} alt="DynamoDB Icon Image"/>
-                        <p>DynamoDB</p>
-                    </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={reactIcon} alt="React Icon Image"/>
+                    <p>React</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={jsIcon} alt="JavaScript Icon Image"/>
+                    <p>JavaScript</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={htmlIcon} alt="HTML Icon Image"/>
+                    <p>HTML</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={cssIcon} alt="CSS Icon Image"/>
+                    <p>CSS</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={sqlIcon} alt="SQL Icon Image"/>
+                    <p>SQL</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={dynamodbIcon} alt="DynamoDB Icon Image"/>
+                    <p>DynamoDB</p>
+                </Grid>
             </Grid>       
         </div>
     )
