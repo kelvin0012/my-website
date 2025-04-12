79eeb309f19fdf39bc09142ef894fb673d59c1de
kelvin0012 <kelvin.at.thesky@gmail.com>
2025-04-12 22:02:09 +1000

    Added new skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,14 +10,25 @@ import cssIcon from "../assets/cssIcon.png";
 import jsIcon from "../assets/jsIcon.png";
 import reactIcon from "../assets/reactIcon.png";
 import dynamodbIcon from "../assets/aws-dynamodb.svg";
+import nodeIcon from "../assets/nodejs.png"
+import javaIcon from "../assets/javaIcon.png"
+import mysqlIcon from "../assets/mysqlIcon.png"
 import Tooltip from '@mui/material/Tooltip';
 
 export const Skills = () => {
     return (
         <div className="skills">
             <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2}>
-                <Grid item xs={12}>
+                <Grid item xs={12} style={{ paddingBottom: '10px' }}>
                     <h2 className="skillsTitle">SKILLS</h2>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={pythonIcon} alt="Python Icon Image" />
+                    <p>Python</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={javaIcon} alt="Java Icon Image" />
+                    <p>Java</p>
                 </Grid>
                 <Grid item xs={6} sm={3} md={1}>
                     <img className="skillsIcon" src={reactIcon} alt="React Icon Image" />
@@ -28,6 +39,10 @@ export const Skills = () => {
                     <p>JavaScript</p>
                 </Grid>
                 <Grid item xs={6} sm={3} md={1}>
+                    <img className="skillsIcon" src={nodeIcon} alt="Node Icon Image" />
+                    <p>Node.js</p>
+                </Grid>
+                <Grid item xs={6} sm={3} md={1}>
                     <img className="skillsIcon" src={htmlIcon} alt="HTML Icon Image" />
                     <p>HTML</p>
                 </Grid>
@@ -36,8 +51,8 @@ export const Skills = () => {
                     <p>CSS</p>
                 </Grid>
                 <Grid item xs={6} sm={3} md={1}>
-                    <img className="skillsIcon" src={sqlIcon} alt="SQL Icon Image" />
-                    <p>SQL</p>
+                    <img className="skillsIcon" src={mysqlIcon} alt="MySQL Icon Image" />
+                    <p>MySQL</p>
                 </Grid>
                 <Grid item xs={6} sm={3} md={1}>
                     <img className="skillsIcon" src={dynamodbIcon} alt="DynamoDB Icon Image" />
